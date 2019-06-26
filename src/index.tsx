dc77ce8e10721d56059ef3798a8f8939125a066b
Kris Burke <kris.burke611@gmail.com>
2019-06-25 21:07:32 -0400

    Add styled-components, linting and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './utils/serviceWorker';
+
+import App from './app/App';
+
+import './assets/styles/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
